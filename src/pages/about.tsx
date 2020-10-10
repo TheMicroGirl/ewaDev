72c8a1d7ae29cf90fc0a24a25965fd6af607ebe2
TheMicroGirl <e.grabowiecka@outlook.com>
2020-10-10 22:01:05 +0100

    changing to gatsby cuz am learing react now

---
+++ src/pages/about.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+const About = () => {
+  return (
+    <div style={{ color: `#ffc800` }}>
+      <h1>This is the about page</h1>
+      <p>What a world.</p>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+    </div>
+  )
+}
+
+export default About
